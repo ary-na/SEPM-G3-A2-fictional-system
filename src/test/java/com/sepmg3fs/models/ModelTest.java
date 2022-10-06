37c6bde24ef3c9155a6cea8f19c08521f6c72824
Arian N. Yamchelo <ariannajafi@outlook.com>
2022-10-07 10:55:32 +1100

    Add JUnit to class path and create ModelTest class java file

---
+++ src/test/java/com/sepmg3fs/models/ModelTest.java
@@ -0,0 +1,24 @@
+package test.java.com.sepmg3fs.models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ModelTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void testModelObject(){
+
+    }
+}
