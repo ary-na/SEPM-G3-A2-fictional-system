02a03bc7788d4d13b11e3bfca81a81fc7da9d405
s3886861 <s3886861@student.rmit.edu.au>
2022-10-08 18:34:04 +0800

    added junit testing

--- src/test/java/com/sepmg3fs/models/ModelTest.java
+++ src/test/java/com/sepmg3fs/models/ModelTest.java
@@ -1,24 +1,68 @@
 package test.java.com.sepmg3fs.models;
 
+import main.java.com.sepmg3fs.models.Model;
+import main.java.com.sepmg3fs.models.Staff;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+
 import static org.junit.Assert.*;
 
 public class ModelTest {
 
-    @Before
-    public void setUp(){
+    private Model model;
+    private Staff user1;
 
+    @Before
+    public void setUp() {
+        model = new Model();
+        user1 = new Staff("aa@aa.com", "Varshan Shankar", "0410111222", "Vv01234567890123456789");
+        model.createAccount(user1.getEmailAddress(), user1.getFullName(), user1.getPhoneNumber(), user1.getPassword());
+        model.setCurrentUser(user1);
+        model.createTicket("test", "high");
     }
 
     @After
-    public void tearDown(){
-
+    public void tearDown() {
+        model = null;
+        user1 = null;
     }
 
     @Test
-    public void testModelObject(){
+    public void testVerifyLogin() {
+        assertTrue(model.verifyLogin(user1.getEmailAddress(), user1.getPassword()));
+    }
 
+    @Test
+    public void testChangePassword() {
+        model.changePassword(user1.getEmailAddress(), "Aa12345678901234567890");
+        model.getCurrentUser().setPassword("Aa12345678901234567890");
+        assertEquals("Aa12345678901234567890", model.getCurrentUser().getPassword());
+    }
+
+    @Test
+    public void testCreateTicket() {
+        assertNotNull(user1.getTickets().get(0));
+    }
+
+    @Test
+    public void testValidateEmail() {
+        assertTrue(model.validateEmail(user1.getEmailAddress()));
+    }
+
+    @Test
+    public void testValidatePhoneNumber() {
+        assertTrue(model.validatePhoneNumber(user1.getPhoneNumber()));
+    }
+
+    @Test
+    public void testValidatePassword() {
+        assertTrue(model.validatePassword(user1.getPassword()));
+    }
+
+    @Test
+    public void testValidateSeverity() {
+        assertTrue(model.validateSeverity("low"));
     }
 }
