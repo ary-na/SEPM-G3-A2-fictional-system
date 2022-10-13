e25d9cff3fe9e01c20edbdc9beae2671f78255bc
robbilee00 <62370508+robbilee00@users.noreply.github.com>
2022-10-13 22:14:04 +1100

    Added UI pathway for selected ticket menu

--- src/main/java/com/sepmg3fs/FrontEndTechnician.java
+++ src/main/java/com/sepmg3fs/FrontEndTechnician.java
@@ -56,8 +56,21 @@ public class FrontEndTechnician {
 
         System.out.println(menu);
     }
+  //Display ticket menu
+    private void displayTicketMenu() {
+        String menu = """
 
-    // View all tickets
+                What would you like to do with this ticket
+
+                [1] Change Severity
+                [2] Change Status
+                [3] Back to last Menu
+                """;
+
+        System.out.println(menu);
+    }
+
+    // View all and select a ticket
     private void viewAllTickets() {
         var users = this.backend.getAllUsers();
 
@@ -72,28 +85,47 @@ public class FrontEndTechnician {
 
             selection = getInput("Type ticket ID or exit: ");
 
-
+            //Get users selection and load ticket menu for selected ticket  if selection valid
             for (User staff : users.values()) {
             	//Only casting if user is staff and not technicians
             	if (staff.getClass().getName().equals(Staff.class.getName())){
-                changeStatus(((Staff) staff).getTickets(), selection);
+                processTicketMenu(((Staff) staff).getTickets(), selection.toLowerCase());
+                selection = "exit";
             	}
 
             }
 
-
+            //If selection is invalid or exit follow path below
             switch (selection.toLowerCase()) {
-
-
+            	
                 case "exit" -> exit = true;
                 default -> System.err.println("\nSelect a valid menu option\n");
             }
 
         } while (!exit);
     }
+    
+    private void processTicketMenu(ArrayList<Ticket> tickets, String Id) {
+        String selection;
+        var exit = false;
+
+            this.displayTicketMenu();
+            selection = getInput("Select an option: ");
+
+            switch (selection) {
+                case "1" -> changeSeverity(tickets, Id);
+                case "2" -> changeStatus(tickets, Id);
+                case "3" -> {
+                }
+                default -> System.err.println("\nSelect a valid menu option\n");
+            }
+
+    }
+
 
 
     public void changeStatus(ArrayList<Ticket> tickets, String Id) {
+    	
     	for (Ticket item : tickets) {
     		if(item.getId().equals(Id)) {
     			System.out.println("Status changed on ticket " + Id);
